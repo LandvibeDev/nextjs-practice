1ab8ba5e5b39156f0566b55302335ebbb1c8924a
lukas-15 <lukas@payapcorp.com>
2021-08-01 15:34:09 +0900

    styled-jsx

--- pages/users/[name].jsx
+++ pages/users/[name].jsx
@@ -1,13 +1,39 @@
+import {css} from "styled-jsx/css";
+
+const style = css`
+  h2 {
+    margin-left: 20px;
+  }
+
+  .user-bio {
+    margin-top: 12px;
+    font-style: italic;
+  }
+`
+
 const name = ({user}) => {
-    const username = user && user.name;
-    return <div>{username}</div>
+    return <>
+        {
+            user ? (
+                    <div>
+                        <h2>{user.name}</h2>
+                        <p className={"user-bio"}>{user.bio}</p>
+                    </div>
+                )
+                :
+                (
+                    <div>유저 정보가 없습니다</div>
+                )
+        }
+        <style jsx>{style}</style>
+    </>
 }
 
-export const getServerSideProps = async (props) =>{
+export const getServerSideProps = async (props) => {
     const {name} = props.query
     try {
         const res = await fetch(`https://api.github.com/users/${name}`)
-        if(res.status === 200) {
+        if (res.status === 200) {
             const user = await res.json()
             return {
                 props: {user}
@@ -15,7 +41,7 @@ export const getServerSideProps = async (props) =>{
         }
 
         return {
-            props:{}
+            props: {}
         }
     } catch (e) {
         console.error(e)
