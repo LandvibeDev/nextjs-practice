5b3ec86a0ec88d2598eb85d4866bb2d5ec9578bd
lukas-15 <lukas@payapcorp.com>
2021-08-01 12:56:35 +0900

    practice router

---
+++ pages/tomato.jsx
@@ -0,0 +1,17 @@
+import Link from "next/link"
+
+const Child = () => {
+    return <p>Move to '/tomato'</p>
+}
+
+const Tomato = () => {
+    return <div>
+        <Link href={"/"}>
+            <a>
+                <Child/>
+            </a>
+        </Link>
+    </div>
+}
+
+export default Tomato
